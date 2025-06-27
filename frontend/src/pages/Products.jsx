39affae46cad7c4049543cb73d17804424ae65e4
TayrineSoares <tayrinecristina@hotmail.com>
2025-06-27 17:06:43 -0400

    test routes and display all products on frontend

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+
 const Products = () => {
+  const [allProducts, setAllProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/products')
+      .then((res) => res.json()) // Parse the JSON response
+      .then((data) => setAllProducts(data))
+      .catch((err) => console.error(err));
+  }, []); // Empty array = runs only once on mount
+
   return (
     <div>
       <h1>Products Page!</h1>
+      {allProducts.map((product) => (
+        <div key={product.id}>
+          <h3>{product.slug}</h3>
+          <p>{product.description}</p>
+          <h3>${(product.price_cents / 100).toFixed(2)}</h3>
+          <hr/>
+
+        </div>
+      ))}
       
     </div>
   )
