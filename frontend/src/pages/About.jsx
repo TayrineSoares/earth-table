32fa78fefa5a786936d0bc7254bd9c248b677b98
TayrineSoares <tayrinecristina@hotmail.com>
2025-07-03 18:13:52 -0400

    merge after pull and adds text to about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,10 +2,36 @@
 const About = () => {
   return (
     <div>
+      <div>
+        <h1>Our Vision</h1>
+        <p> At Earth Table, our vision is simple: to nourish people with food that is as wholesome as it is delicious. </p>
+        <p> We specialize in holistic meal prep and personal chef services focused on your well-being and peace of mind. </p>
 
-      <h1>About Page for Earth table</h1>
+        <p> We are committed to using only the highest-quality ingredients: </p>
+        <li>Seed oil free </li>        
+        <li>Organic </li> 
+        <li>Grass-fed, pasture-raised meat and dairy </li>
+        <li>Gluten-free</li>
+    
+        
+          <p> Every meal we create is crafted to support your health, respect the planet, and celebrate the pure flavors of real food.</p> 
+
+        <p> We believe that eating well is not just a trend. It is a lifestyle rooted in care, quality, and conscious choices.</p>
+      </div>
+
+      <div>
+        <h1>Meet our founder </h1>
+        <p>Chef Selena is the creative force behind Earth Table's vibrant, holistic cuisine. </p> 
+        <p> With a passion for crafting meals that nurture both body and soul, she blends culinary artistry with a deep commitment to wellness. </p>
+
+        <p> Drawing on years of experience in high-end kitchens and personal chef services, her food is all about clean ingredients, bold flavors, and luxurious simplicity. </p> 
+
+        <p> Whether she’s designing a personalized meal plan or preparing an elegant dinner, Selena believes that eating well should feel joyful and effortless. </p>
+        
+      </div>
       
     </div>
+    
   )
 };
 
