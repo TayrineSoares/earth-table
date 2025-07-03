556195b722ef37864a0b0e8507ee2576c82b3914
TayrineSoares <tayrinecristina@hotmail.com>
2025-07-03 18:43:17 -0400

    Style about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,34 +1,38 @@
+import '../styles/About.css'
+
 
 const About = () => {
   return (
-    <div>
-      <div>
+    <div className="about-page">
+      <section className="vision-section">
         <h1>Our Vision</h1>
-        <p> At Earth Table, our vision is simple: to nourish people with food that is as wholesome as it is delicious. </p>
-        <p> We specialize in holistic meal prep and personal chef services focused on your well-being and peace of mind. </p>
+        <p> At Earth Table, our vision is simple: to <strong><em>nourish</em></strong> people with food that is as wholesome as it is delicious. </p>
+        <p> We specialize in <strong> holistic meal prep and personal chef services </strong> focused on your well-being and peace of mind. </p>
 
-        <p> We are committed to using only the highest-quality ingredients: </p>
-        <li>Seed oil free </li>        
-        <li>Organic </li> 
-        <li>Grass-fed, pasture-raised meat and dairy </li>
-        <li>Gluten-free</li>
-    
+        <p> We are committed to using only the <strong>highest-quality</strong> ingredients: </p>
+        <ul>
+          <li>✔ Seed oil free</li>
+          <li>✔ Organic</li>
+          <li>✔ Grass-fed, pasture-raised meat and dairy</li>
+          <li>✔ Gluten-free</li>
+        </ul>
         
           <p> Every meal we create is crafted to support your health, respect the planet, and celebrate the pure flavors of real food.</p> 
 
-        <p> We believe that eating well is not just a trend. It is a lifestyle rooted in care, quality, and conscious choices.</p>
-      </div>
+        <p> We believe that <strong>eating well is not just a trend.</strong> </p>
+        <p>It is a lifestyle rooted in <strong>care, quality, and conscious choices.</strong></p>
+      </section>
 
-      <div>
+      <section className="founder-section">
         <h1>Meet our founder </h1>
-        <p>Chef Selena is the creative force behind Earth Table's vibrant, holistic cuisine. </p> 
-        <p> With a passion for crafting meals that nurture both body and soul, she blends culinary artistry with a deep commitment to wellness. </p>
+        <p>Chef <strong> Selena Campoli</strong> is the creative force behind Earth Table's vibrant, holistic cuisine. </p> 
+        <p> With a passion for crafting <strong><em>meals that nurture both body and soul</em></strong>, she blends culinary artistry with a deep commitment to wellness. </p>
 
-        <p> Drawing on years of experience in high-end kitchens and personal chef services, her food is all about clean ingredients, bold flavors, and luxurious simplicity. </p> 
+        <p> Drawing on years of experience in high-end kitchens and personal chef services, her food is all about <strong>clean ingredients, bold flavors, and luxurious simplicity.</strong> </p> 
 
-        <p> Whether she’s designing a personalized meal plan or preparing an elegant dinner, Selena believes that eating well should feel joyful and effortless. </p>
+        <p> Whether she's designing a personalized meal plan or preparing an elegant dinner, Selena believes that eating well should feel joyful and effortless. </p>
         
-      </div>
+      </section>
       
     </div>
     
