3ae1b4ab76fbbd59d563487e16a4f8c532237e3c
TayrineSoares <tayrinecristina@hotmail.com>
2025-07-11 21:18:27 -0400

    add logic to the POST LOGIN route

--- backend/src/routes/loginRoutes.js
+++ backend/src/routes/loginRoutes.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const supabase = require('../../supabase/db');
 
 const { getAllUsers, getUserById } = require('../queries/user')
 
+// Supabase Auth handles login and returns you the user’s id and info if the email + password are correct.
+// POST /login
 
-// GET /login
+router.post('/', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Attempt to sign in the user using Supabase Auth
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    // If there was an error signing in (e.g. invalid email/password), return 401 Unauthorized
+    if (error) {
+      return res.status(401).json({error: error.message});
+    }
+
+    // Extract the logged-in user's unique Supabase ID
+    return res.status(200).json({
+      user:data.user,
+      session:data.session,
+    }); 
+
+  } catch (err) {
+    console.error(err.message); 
+    return res.status(500).json({error: "Server error"})
+  }
+}); 
 
 
 
