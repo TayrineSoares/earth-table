96a77d9a70bdff2cab2fddc2109f9abaab4e78da
TayrineSoares <tayrinecristina@hotmail.com>
2025-07-11 21:08:22 -0400

    Create login backend route

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const categoriesRouter = require('./src/routes/categoriesRoutes');
 const productsRouter = require('./src/routes/productsRoutes');
 const ordersRouter = require('./src/routes/ordersRoutes')
 const usersRouter = require('./src/routes/usersRoutes')
+const loginRouter = require('./src/routes/loginRoutes')
 
 
 const app = express();
@@ -28,6 +29,9 @@ app.use('/products', productsRouter);
 app.use('/orders', ordersRouter);
 
 app.use('/users', usersRouter);
+
+app.use('/login', loginRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
---
+++ backend/src/routes/loginRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const { getAllUsers, getUserById } = require('../queries/user')
+
+
+// GET /login
+
+
+
+module.exports = router;
