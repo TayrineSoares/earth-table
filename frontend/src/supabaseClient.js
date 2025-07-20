eb97d1016cdd0b6c3025aa0b2dcaafeb49b04607
TayrineSoares <tayrinecristina@hotmail.com>
2025-07-20 16:39:29 -0400

    update anon supabase

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,8 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ujmwwnnyylsqyqqlrykx.supabase.co'; 
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqbXd3bm55eWxzcXlxcWxyeWt4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4NzQ1MzQsImV4cCI6MjA2NjQ1MDUzNH0.h47iGnsijAMiWzBnWWnxavSrY4DjsDWU5bTA2o1sIzY'; // ANON PUBLIC KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
