21388bfd425ca3d1513e5bae00329ba89a5325bc
TayrineSoares <tayrinecristina@hotmail.com>
2025-07-20 13:52:56 -0400

    create reset password form structure

--- frontend/src/pages/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword.jsx
@@ -1,9 +1,30 @@
-
+import { useState } from "react";
 
 const ResetPassword = () => {
+  const [ email, setEmail ] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Reset Password for", email);
+  }
+
   return (
-    <div>
+    <div className="reset-password page">
       <h1>Reset password page</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+        type="email"
+        placeholder="Enter your email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+          
+        />
+        <br></br>
+        <br></br>
+        <button type="submit">Send reset Link </button>
+      </form>
+
     </div>
   )
 };
