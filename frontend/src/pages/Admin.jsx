719191e64fa8663e2e5b065863c951fe2daeb934
TayrineSoares <tayrinecristina@hotmail.com>
2025-07-22 15:57:31 -0400

    update use effect to only allow access to /admin page if the user is defined as admin. otherwise, redirect to homepage

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -6,15 +6,43 @@ const Admin = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem('user'));
-    console.log('This is the stored user', storedUser);
-  }, []);
+    const fetchUser = async () => {
+      const storedUser = JSON.parse(localStorage.getItem('user'));
+
+      if (!storedUser ) {
+        navigate('/');
+        return;
+      }
+
+      try {
+        const res = await fetch(`http://localhost:8080/users/${storedUser.id}`);
+        const data = await res.json();
+
+        if (!res.ok || !data.is_admin) {
+          navigate('/');
+
+        } else {
+          setUser(data);
+        }
+      } catch (err) {
+        console.error("Error fetching user from backend:", err);
+        navigate('/');
+      }
+    };
+    
+    fetchUser();
+  
+  }, [navigate]);
+
+  if (!user) return null;
 
 
   return (
     <div>
 
       <h1>Admin Page!</h1>
+      <p>Welcome, {user.first_name || user.email}</p>
+      
       
     </div>
   )
