0965e62631c6fdf4017368fadbb6188d148d6d58
Dartanhan Ramos <alessandroramosd@gmail.com>
2025-07-23 18:36:37 -0400

    finishing home page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,39 @@
 import { Instagram } from 'lucide-react';
+import { Link } from "react-router-dom";
 import '../styles/Footer.css';
+import logoNoBackground from "../assets/images/logoNoBackground.png";
+import blackLogo from "../assets/images/blackLogo.png";
 
 const Footer = () => {
   return (
     <footer className="footer">
-       <div className="social-icons">
+      <div className="logo-footer">
+          <img
+            src={blackLogo}
+            className="footer-earth-logo"
+            alt="Earth Logo"
+          />
+          <Link to="/">
+            <p
+              className="footer-web-title"
+            >EARTH TABLE CO</p>
+          </Link>
+      </div>
+
+      <div className='copyright-contatiner'>
+        <p className='copyright'>© 2025 EARTH TABLE CO</p>
+      </div>
+
+
+        <div className='footer-number-container'>
+          <p className='footer-number'>(555) 123-4567</p>
+        </div>
+        
+        <div className='footer-address-container'>
+          <p className='footer-address'>123 Fake street, Toronto, ON</p>
+        </div>
+
+      <div className="social-icons">
         <a 
           href="https://www.instagram.com/earthtable_co/"
           target="_blank" 
@@ -13,16 +42,6 @@ const Footer = () => {
         >
           <Instagram size={25} />
         </a>
-      </div>
-
-      <div className='footer-logo'>
-        <span>Earth Table</span>
-
-      </div>
-      <div className="footer-info">
-        
-        <span>123 Fake street, Toronto, ON</span>
-        <span>(555) 123-4567</span>
       </div>
      
     </footer>
