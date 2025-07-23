83ede60bef4f871fd9763a03e332b468497335e2
TayrineSoares <tayrinecristina@hotmail.com>
2025-07-23 14:10:11 -0400

    fetch and display all products title and price on user admin tab

--- frontend/src/components/ProductAdmin.jsx
+++ frontend/src/components/ProductAdmin.jsx
@@ -1,9 +1,38 @@
+import { useEffect, useState } from 'react';
 import { fetchAllProducts, fetchAllCategories } from '../helpers/adminHelpers';
 
 const ProductAdmin = () => {
+  const [products, setProducts] = useState([]); 
+
+  useEffect(() => {
+    const loadProducts = async () => {
+      try {
+        const data = await fetchAllProducts();
+        setProducts(data);
+      } catch (err) {
+        console.error("Error fetching products:", err);
+      }
+    };
+    loadProducts(); 
+  },[]);
+
+  
   return (
     <div>
-      <h1>Produc admin tab </h1>
+      <h1>Produc Management </h1>
+      {products.length === 0 ? (
+        <p>No products found.</p>
+      ) : (
+        <ul>
+          {products.map(product => (
+            <li key={product.id}>
+              {product.slug} - ${(product.price_cents / 100).toFixed(2)}
+            </li>
+
+          ))}
+        </ul>
+      
+      )}
       
     </div>
   )
