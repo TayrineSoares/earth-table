12352a66baccf5e5b597cc28cc7441e6a46d523d
TayrineSoares <tayrinecristina@hotmail.com>
2025-08-05 10:23:43 -0400

    create basic confirmation page

--- frontend/src/pages/Confirmation.jsx
+++ frontend/src/pages/Confirmation.jsx
@@ -1,12 +1,17 @@
-
+import { Link } from 'react-router-dom';
 
 const Confirmation = () => {
   return (
     <div>
-      <h1>Confirmation Page!</h1>
-      
+      <h1>Thank You for Your Order!</h1>
+
+      <p>Your payment was successful.</p>
+      <p>A confirmation email has been sent to your inbox.</p>
+
+
+      <Link to="/">Back to Home</Link>
     </div>
-  )
+  );
 };
 
 export default Confirmation;
