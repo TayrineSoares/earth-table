d3b4532262f2b737b4e726fdebb73357268e52d7
TayrineSoares <tayrinecristina@hotmail.com>
2025-08-05 13:55:16 -0400

    improve confirmation page

--- frontend/src/pages/Confirmation.jsx
+++ frontend/src/pages/Confirmation.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { fetchOrderBySessionId } from '../helpers/orderHelpers';
 import { Link } from 'react-router-dom';
+import "../styles/Confirmation.css"
 
 const Confirmation = () => {
   const [searchParams] = useSearchParams();
@@ -16,7 +17,7 @@ const Confirmation = () => {
       const data = await fetchOrderBySessionId(sessionId);
       setOrder(data); // could still be null if not found
       setLoading(false);
-      console.log("This is the order" , order);
+      //console.log("This is the order" , order);
     };
 
     getOrder();
@@ -37,23 +38,28 @@ const Confirmation = () => {
 
   return (
     <div>
-      <h1>Thank You for Your Order!</h1>
+      <h2>Thank You for Your Order!</h2>
       <p>Order ID: {order.id}</p>
       <p>Status: {order.status}</p>
-      <p>Total: ${(order.total_cents / 100).toFixed(2)}</p>
-
-      <h2>Items:</h2>
-      <ul>
-        {order.products?.map((product, index) => (
-          <li key={index}>
-            {product.quantity}x {product.slug} – ${(product.unit_price_cents / 100).toFixed(2)}
-          </li>
-        ))}
-      </ul>
-
       
 
-      <p>A confirmation email has been sent to you.</p>
+      <h3>Items:</h3>
+      {order.products?.length > 0 ? (
+        <ul>
+          {order.products.map((product, index) => (
+            <li key={index}>
+              {product.quantity}x {product.slug} - ${(product.unit_price_cents / 100).toFixed(2)}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No items found in this order.</p>
+      )}
+      
+      <p>Total: ${(order.total_cents / 100).toFixed(2)}</p>
+      
+
+      <p>A confirmation email has been sent to {order.buyer_email || 'your email address'}.</p>
       <Link to="/">Back to Home</Link>
     </div>
   );
