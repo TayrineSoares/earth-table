bd2a0ef9add7775f3e399784807bd65b1b807b3b
TayrineSoares <tayrinecristina@hotmail.com>
2025-08-12 11:50:16 -0400

    add date pickup selector

--- frontend/src/components/PickupSelector.jsx
+++ frontend/src/components/PickupSelector.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import { useState } from "react";
 
 const PickupSelector = () => {
+  const [pickupDate, setPickupDate] = useState("");
+  const [pickupTime, setPickupTime] = useState("");
+
+  // calculate minimum available date (today + 3 days)
+  const getMinDate = () => {
+    const date = new Date();
+    date.setDate(date.getDate() + 3);
+    return date.toISOString().split("T")[0]; // format: YYYY-MM-DD  
+
+  }
+
+
+
+
   return (
     <div>
       <h1>Pickup Selector component </h1>
+      {/* Date Picker */}
+      <lable>
+        Pickup Date:
+        <input
+          type="date"
+          value={pickupDate}
+          min={getMinDate()}
+          onChange={(e) => setPickupDate(e.target.value)}
+        />       
+      </lable>
       
     </div>
   )
